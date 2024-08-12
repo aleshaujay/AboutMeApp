a6760b38a260d20a0ab29855114a1039f51a055a
aleshaujay <aleshaujay@icloud.com>
2024-08-12 23:58:38 +0700

    Now everything will work 🫣 attempt number 2

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Lex Koby on 11.08.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
