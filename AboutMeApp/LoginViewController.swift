0cdacb710f39b9f8f59869162e0a5d2aa647deca
aleshaujay <aleshaujay@icloud.com>
2024-08-14 17:07:33 +0700

    The second attempt 🤬

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  AboutMeApp
 //
 //  Created by Lex Koby on 11.08.2024.
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet var userNameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
+    let user = "Lex"
+    let password = "1111"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = user
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameField.text == user, passwordField.text == password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password"
+            )
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func forgotUserName() {
+        showAlert(title: "Oops!", message: "Your name is \(user) ✌️")
+    }
+    
+    @IBAction func forgotPassword() {
+        showAlert(title: "Oops!", message: "Your password is \(password) 👍")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameField.text = ""
+        passwordField.text = ""
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
-
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -7,14 +7,19 @@
 
 import UIKit
 
-class WelcomeViewController: UITabBarController {
+final class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        welcomeLabel.text = "Welcome, \(userName ?? "")😍"
     }
     
-
-
+    @IBAction func logOutButtonTapped() {
+        dismiss(animated: true)
+        // я помню что ещё был способ сделать выход на предыдущую страницу, но я решил сделать тут вот так 🤷‍♂️
+    }
 }
